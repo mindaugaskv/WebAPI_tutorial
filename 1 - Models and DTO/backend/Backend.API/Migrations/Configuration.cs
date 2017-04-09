b846cc4984b6e3eb6cdfd3431d5e8e7d0f2cff6e
MKvederas <mkv@nrd.lt>
2017-04-09 11:08:08 +0300

    removing database files

--- 1 - Models and DTO/backend/Backend.API/Migrations/Configuration.cs
+++ 1 - Models and DTO/backend/Backend.API/Migrations/Configuration.cs
@@ -19,33 +19,10 @@ namespace Backend.API.Migrations
             if (!context.LegoParts.Any())
             {
                 context.LegoParts.AddRange(new List<LegoPart>() {
-                    new LegoPart(){ Name = "Part 1", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 2", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 3", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 4", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 5", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 6", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 7", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 8", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 10", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 9", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 11", Color = "Red", Size = "Big", Shape = "Square"},
-                    new LegoPart(){ Name = "Part 12", Color = "Red", Size = "Big", Shape = "Square"},
+                    new LegoPart(){ Name = "Brick", Color = "Red", Size = "Big", Shape = "Square", ImageUrl = "https://www.bricklink.com/PL/11293pb001.jpg"},
+                    new LegoPart(){ Name = "Double brick", Color = "Red", Size = "Big", Shape = "Square", ImageUrl = "https://www.bricklink.com/PL/87615pb05.jpg"},
                 });
             }
-
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
