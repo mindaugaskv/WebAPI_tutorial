1b6c09ee531c360e4eb64149a8c157640a30927a
MKvederas <mkv@nrd.lt>
2017-04-09 10:26:20 +0300

    Setting up startup template

--- 0 - Start/backend/Backend.API/Global.asax.cs
+++ 0 - Start/backend/Backend.API/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using Newtonsoft.Json.Serialization;
+﻿using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,25 +18,7 @@ namespace Backend.API
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            
-
-            var config = GlobalConfiguration.Configuration;
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
-                new CamelCasePropertyNamesContractResolver();
-            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
-
-            
-            Mapper.Initialize(cfg => {
-                cfg.CreateMap<Enties.LegoPart, Models.LegoPartDto>()
-                    .ForMember(m => m.Id, s => s.Ignore())
-                    .ForMember(m => m.Name, s => s.MapFrom(d => $"{d.Name}  {d.Shape}"))
-                    ;
-
-                cfg.CreateMap<Models.LegoPartDto, Enties.LegoPart>();
-                
-            });
+            BundleConfig.RegisterBundles(BundleTable.Bundles);            
         }
     }
 }
